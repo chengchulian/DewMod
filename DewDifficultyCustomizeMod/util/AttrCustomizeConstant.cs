c52b8d030c17532a4bb1f402b8862355542d217a
chengchulian <chengchulian@qq.com>
2025-10-22 19:04:19 +0800

    chore(version): 更新插件版本号至1.0.5
    - 将 PluginVersion 常量从 "1.0.4" 更新为 "1.0.5"

--- DewDifficultyCustomizeMod/util/AttrCustomizeConstant.cs
+++ DewDifficultyCustomizeMod/util/AttrCustomizeConstant.cs
@@ -15,7 +15,7 @@ public static class AttrCustomizeConstant
     public const string PluginGuid = $"com.{PluginAuthor}.DewMod.{PluginName}";
     public const string PluginAuthor = "chengchulian";
     public const string PluginName = "DewDifficultyCustomizeMod";
-    public const string PluginVersion = "1.0.4";
+    public const string PluginVersion = "1.0.5";
     
     
 }
