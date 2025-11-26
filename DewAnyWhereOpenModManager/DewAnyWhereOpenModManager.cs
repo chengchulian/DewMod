eeeb1ec628cb2303ce83c134b55a2c27528b73e9
chengchulian <chengchulian@qq.com>
2025-11-26 23:58:21 +0800

    feat(mod): 实现在任意位置打开Mod管理器的功能
    - 新增全局快捷键(F1)用于打开/关闭Mod管理器
    - 添加本地化支持，可加载i18n文件夹中的多语言JSON文件
    - 实现UI文本的自动本地化替换功能
    - 配置界面支持自定义打开快捷键
    - 提供中英文默认本地化资源文件
    - 注册新项目到解决方案并完成基础框架搭建

---
+++ DewAnyWhereOpenModManager/DewAnyWhereOpenModManager.cs
@@ -0,0 +1,25 @@
+﻿using DewAnyWhereOpenModManager.config;
+using UnityEngine;
+
+namespace DewAnyWhereOpenModManager;
+
+public class DewAnyWhereOpenModManager : ModBehaviour
+{
+    public static DewAnyWhereOpenModManager Instance;
+    
+    
+    public PluginConfig config = new PluginConfig();
+
+    private void Start()
+    {
+        Instance = this;
+        LocalizationSource.Init();
+    }
+    private void Update()
+    {
+        if (!Input.GetKeyDown(config.OpenKey)) return;
+        
+        var active = UI_ModManager.instance.gameObject.activeSelf;
+        UI_ModManager.instance.gameObject.SetActive(!active);
+    }
+}
---
+++ DewAnyWhereOpenModManager/config/PluginConfig.cs
@@ -0,0 +1,22 @@
+﻿using ExternalPropertyAttributes;
+using UnityEngine;
+
+namespace DewAnyWhereOpenModManager.config;
+
+public class PluginConfig : ModConfig
+{
+    [Header("Header.Mod.Config")]
+    [LabelText("LabelText.OpenKey")]
+    [Description("Description.OpenKey")]
+    public KeyCode OpenKey = KeyCode.F1;
+
+    
+    
+    public override void BuildWidgets(Transform parent, out SafeAction onChanged, out SafeAction requestUpdate)
+    {
+
+        base.BuildWidgets(parent, out onChanged, out requestUpdate);
+        // 本地化UI
+        LocalizationSource.LocalizeUI(parent);
+    }
+}
