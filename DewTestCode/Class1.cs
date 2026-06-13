5aa339c44c8db6cd310d27117a8d591cd71413f4
chengchulian <chengchulian@qq.com>
2026-06-14 01:03:34 +0800

    chore(gitignore): 忽略构建脚本 (#6)
    - 将 build.ps1 加入 .gitignore 防止误提交

---
+++ DewTestCode/Class1.cs
@@ -0,0 +1,13 @@
+namespace DewTestCode;
+
+public class Class1
+{
+    public static void Main(string[] args)
+    {
+        Dew.CreateActor<Shrine_CorruptedChaos>(DewConsoleCommands.GetCursorWorldPos(), null);
+    }
+        
+
+
+        
+}
